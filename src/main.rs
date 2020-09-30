df76582e2c2c06ce892008471c07d676e3b5f767
John Nguyen <knightofthealtar64@gmail.com>
2020-09-30 12:41:24 -0700

    Add main.rs and start the opener

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Hello, kind player!\nWelcome to Rustaceoneer!\nChoose your class!");
+    
+    let mut class = String.new();
+
+    io:stdin()
+	.read_line(&mut class)
+	.expect("Failed to read line");
+
+        
+}
+
