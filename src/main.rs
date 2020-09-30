193cf3057cd2417c4c88a1a9f859840ae573edfc
John Nguyen <knightofthealtar64@gmail.com>
2020-09-30 13:59:33 -0700

    Remove Java idiosyncracies

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use std::cmp::Ordering;
 fn main() {
     println!("Hello, kind player!\nWelcome to Rustaceoneer!\nChoose your class!");
     
-    let mut class = String.new();
+    let mut class = String::new();
 
-    io:stdin()
+    io::stdin()
 	.read_line(&mut class)
 	.expect("Failed to read line");
 
-        
+    if let class = "fighter" {
+    	println!("Nice choice!  that's the only one implemented, lol");
+    };
 }
 
